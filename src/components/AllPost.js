f435d09adf036a96a02e37bda0768dd7be68bcfc
Vener Guevarra <vnrguevarra@gmail.com>
2018-10-19 11:24:53 +0800

    Initial commit

---
+++ src/components/AllPost.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Post from './Post';
+import EditPostForm from './EditPostForm';
+
+
+class AllPost extends Component {
+    render() {
+        return (
+            <div className="post-container">
+                <h1 className="post_heading">All Posts</h1>
+                {this.props.posts.map((post) => (
+                    <div key={post.id}>
+                        {post.editing ?
+                            <EditPostForm post={post} key={post.id} /> :
+                            <Post key={post.id} post={post} />}
+                    </div>
+                ))}
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        posts: state.postReducer
+    }
+}
+export default connect(mapStateToProps)(AllPost);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {combineReducers, createStore} from 'redux';
+import { Provider } from 'react-redux';
+import postReducer from './reducers/postReducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import './index.css';
+
+ const allReducers  = combineReducers({
+    postReducer: postReducer
+ });
+
+const store = createStore(allReducers);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
